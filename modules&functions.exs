ff2105c2961eb57687af120f0c75e7a7c52966a3
Hussein Omar <husseinomar6190@gmail.com>
2022-08-18 21:44:39 +0300

    passing parametors to functions

--- modules&functions.exs
+++ modules&functions.exs
@@ -6,10 +6,14 @@
 #  2. Modules are typically named using camel case i.e HelloWorld
 #  3. do and end act as brackets where it is a block where code goes
 #  4. Functions are name using snake case
+#  5. You can also pass values to a function
 
 defmodule ModulesFunctions do
   def say_hello do
     IO.puts('Hello world')
+  end
+  def say_whatever(staff) do
+    IO.puts(staff)
   end
 end
 
