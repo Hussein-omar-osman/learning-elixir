d8302dbff743f214e7e548cc40868f2a561e5130
Hussein Omar <husseinomar6190@gmail.com>
2022-08-18 21:16:51 +0300

    atoms and tuples

---
+++ atoms&tuples.exs
@@ -0,0 +1,39 @@
+  #  Atom
+
+:ok
+# i :ok
+# Term
+#   :ok
+# Data type
+#   Atom
+# Reference modules
+#   Atom
+# Implemented protocols
+#   IEx.Info, Inspect, List.Chars, String.Chars
+
+:whatever
+# i :whatever
+# Term
+#   :whatever
+# Data type
+#   Atom
+# Reference modules
+#   Atom
+# Implemented protocols
+#   IEx.Info, Inspect, List.Chars, String.Chars
+
+
+  # Tubles
+
+
+a = {1, 2, 'staff'}
+IO.puts(x)
+# i a
+# Term
+#   {1, 2, 'staff'}
+# Data type
+#   Tuple
+# Reference modules
+#   Tuple
+# Implemented protocols
+#   IEx.Info, Inspect
