cb964d3f260b3ed72bee3edf0192cba7d2ab8d17
Hussein Omar <husseinomar6190@gmail.com>
2022-12-16 23:57:19 +0300

    more about pattern matching & handling unused variables

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -40,10 +40,18 @@ defmodule Cards do
   end
 
   def load(filename) do
-    {status, binary} = File.read(filename)
-    case status do
-      :ok -> :erlang.binary_to_term(binary)
-      :error -> "The file provided does not exist"
+    # Original aproach code
+    # {status, binary} = File.read(filename)
+    # case status do
+    #   :ok -> :erlang.binary_to_term(binary)
+    #   :error -> "The file provided does not exist"
+    # end
+
+    # More pattern matching and handling unused variables with a prefix of underscore
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "The file provided does not exist"
     end
+
   end
 end
