9268cb9f868c6b648423a128f9e1e59cfd934071
Hussein Omar <husseinomar6190@gmail.com>
2022-12-17 15:26:55 +0300

    created docs for the cards module

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,16 +1,10 @@
 defmodule Cards do
   @moduledoc """
-  Documentation for `Cards`.
+    Provides methods for creating and handling deck of cards
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
+  Create a new deck of cards
   """
   def create_deck do
     values = ["Ace", "Two","Three","Four","Five"]
@@ -21,23 +15,35 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Shuffle the created deck of cards
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Checks if a certain card is in a deck
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
-
+  @doc """
+    Deals some given number of cards
+  """
   def deal(deck, many) do
     Enum.split(deck, many)
   end
-
+  @doc """
+    Saves the deck in a file
+  """
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
-
+  @doc """
+    Loads a deck from the filename you provide
+  """
   def load(filename) do
     # Original aproach code
     # {status, binary} = File.read(filename)
@@ -52,6 +58,10 @@ defmodule Cards do
       {:error, _reason} -> "The file provided does not exist"
     end
   end
+
+  @doc """
+    Create a deck , shuffle, and deal the cards at a given number of hands
+  """
 
   def create_hand(hand_size) do
     # Without the pipe operator
