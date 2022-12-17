052e547acf888c5e009c8378fd1d71d6812b3a7d
Hussein Omar <husseinomar6190@gmail.com>
2022-12-17 17:39:19 +0300

    runnign regular test

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -2,7 +2,14 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "create_deck returns 20 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 20
+  end
+
+  test "hand given length must be equal to the hand returned" do
+    {hand, _rest} = Cards.create_deck |> Cards.shuffle |> Cards.deal(5)
+    hand_length = length(hand)
+    assert hand_length == 5
   end
 end
