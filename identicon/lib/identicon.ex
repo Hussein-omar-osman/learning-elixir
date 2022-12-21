bbdb579e0772dfefbecfb346f3bb4e2e25e3c3a9
Hussein Omar <husseinomar6190@gmail.com>
2022-12-21 15:56:52 +0300

    building the pixel

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,7 +3,7 @@ defmodule Identicon do
   Documentation for `Identicon`.
   """
   def main(input) do
-    input |> hash_input |> pick_color |> build_grid |> filter_odd_squares
+    input |> hash_input |> pick_color |> build_grid |> filter_odd_squares |> build_pixel_map
   end
 
   def hash_input(input) do
@@ -37,6 +37,21 @@ defmodule Identicon do
                 rem(code, 2) == 0
               end
     %Identicon.Image{image | grid: new_grid}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 
 end
