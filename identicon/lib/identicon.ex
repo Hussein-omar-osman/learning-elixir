008a7fa38c8c963197046867d0e560a84074b2bb
Hussein Omar <husseinomar6190@gmail.com>
2022-12-21 16:20:27 +0300

    drawing and saving image

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,7 +3,7 @@ defmodule Identicon do
   Documentation for `Identicon`.
   """
   def main(input) do
-    input |> hash_input |> pick_color |> build_grid |> filter_odd_squares |> build_pixel_map
+    input |> hash_input |> pick_color |> build_grid |> filter_odd_squares |> build_pixel_map |> draw_image |> save_image(input)
   end
 
   def hash_input(input) do
@@ -52,6 +52,22 @@ defmodule Identicon do
     end
 
     %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
   end
 
 end
